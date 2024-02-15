3692b4e6baaa6a0d7b4adadd18b2bbccdcab6f0e
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-15 10:40:47 +0900

    Day2 Lesson4 基本まで終了

---
+++ MyOkashi/safariView.swift
@@ -0,0 +1,18 @@
+//
+//  safariView.swift
+//  MyOkashi
+//
+//  Created by 木村朋広 on 2024/02/15.
+//
+
+import SwiftUI
+
+struct safariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    safariView()
+}
